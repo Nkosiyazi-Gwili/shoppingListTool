b746f3201be5e75c1c16d807f668d32510ce4b20
Nkosiyazi-Gwili <95026019+Nkosiyazi-Gwili@users.noreply.github.com>
2023-06-13 13:36:19 +0200

    Update lib-items.php
    udpade db credentials to local ones

--- lib-items.php
+++ lib-items.php
@@ -52,7 +52,7 @@ class Items {
 
 // (H) DATABASE SETTINGS - CHANGE TO YOUR LOCAL!
 define("DB_HOST", "localhost");
-define("DB_NAME", "test");
+define("DB_NAME", "");
 define("DB_CHARSET", "utf8mb4");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
